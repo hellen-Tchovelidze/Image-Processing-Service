1b2870a35d5978eb3483ba3aa15f962f131a0546
hellen-Tchovelidze <elenechovelidze@gmail.com>
2025-07-23 20:29:08 +0400

    gg

--- src/asw-s3/asw-s3.service.ts
+++ src/asw-s3/asw-s3.service.ts
@@ -21,7 +21,7 @@ export class AswS3Service {
         accessKeyId: process.env.AWS_ACCESS_KEY!,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
       },
-      region: process.env.AWS_REGION,
+      region: process.env.AWS_REGIONN,
     });
   }
 
